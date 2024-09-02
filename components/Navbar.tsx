03ca88e4c1d94dbebb8a2dddcf1ef861026a219e
unknown <aniketkrbiswas239@gmail.com>
2024-09-02 10:28:56 +0530

    local changes

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,23 +1,44 @@
 import React from "react";
-import { View, Text,Image, StyleSheet } from "react-native";
+import { View, Text, Image, StyleSheet } from "react-native";
 
 const Navbar = () => {
     return (
         <View style={{
-            backgroundColor: 'white',
-            padding: 10,
+            flexDirection: 'row',
         }}>
-            <Text style={styles.innerText}> Logo</Text>
+            <View style={{
+                backgroundColor: 'white',
+                padding: 10,
+            }}>
+                <span class="material-symbols-outlined">
+                    menu
+                </span>
+            </View>
+            <View style={{
+                backgroundColor: 'white',
+                padding: 10,
+            }}>
+                <Text style={styles.innerText}> Logo</Text>
+            </View>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
-    innerText:{
-        color:'black',
+    innerText: {
+        color: 'black',
         fontWeight: 'bold',
         fontSize: 25
     }
+    <style>
+        .material - symbols - outlined {
+            font- variation - settings:
+    'FILL' 0,
+    'wght' 400,
+    'GRAD' 0,
+    'opsz' 24
+}
+</style >
 });
 
 export default Navbar;
